50d6908a5d80260c8bd6760222dfb59152a3677a
lorenzoogarciia <94983122+lorenzoogarciia@users.noreply.github.com>
2024-02-21 18:31:14 +0100

    Add files via upload
    Ejercicios básicos con interfaz para Java.

---
+++ ASCII.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class ASCII {
+
+	public static void main(String[] args) {
+		// Programa que transforma un número a su valor en la tabla ASCII
+				String texto=JOptionPane.showInputDialog("Introduce un codigo de la tabla ASCII");
+				//Pasamos el String a int
+				int codigo=Integer.parseInt(texto);
+
+				//Pasamos el codigo a caracter
+				char caracter=(char)codigo;
+
+				System.out.println(caracter);
+			}
+		
+
+}
